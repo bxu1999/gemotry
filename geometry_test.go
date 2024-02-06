128df1511eeecd996f29b1b8086e4f152897db7a
bxu1999 <bxu1999@example.com>
2024-02-06 10:48:26 -0800

    add _test code

--- geometry_test.go
+++ geometry_test.go
@@ -1,3 +1,16 @@
 package geometry
 
+import (
+        "testing"
+)
+
+func TestLength(t *testing.T) {
+
+	point1 := Point{X: 3, Y: 4}
+        expected := 5.0
+
+        if got := point1.Length(); got != expected {
+                t.Errorf("Point{X: 3, Y: 4}= %f, didn't return %f",  got, expected)
+        }
+}
 
